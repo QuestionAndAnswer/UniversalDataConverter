1d433d9f9fd7081d06d462f9778f20651f66e120
Artsiom Miksiuk <artsiom.miksiuk@sap.com>
2016-08-29 18:35:42 +0300

    Initial.

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+	//disable beeping. Tooked from https://github.com/gruntjs/grunt/issues/808#issuecomment-20903038
+	var oldout = process.stdout.write;
+	process.stdout.write = function(msg) {
+	  oldout.call(this, msg.replace('\x07', ''));
+	};
+
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-jsdoc");
+
+	grunt.renameTask("watch", "actualWatch");
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		jsdoc: {
+			dist: {
+				src: ["universal-data-converter.js"],
+				dest: "doc"
+			}
+		},
+		actualWatch: {
+			doc: {
+				files: ["universal-data-converter.js"],
+				tasks: ["jsdoc"]
+			}
+		}
+	});
+
+	grunt.registerTask("watch", ["actualWatch"]);
+	grunt.registerTask("doc", ["jsdoc"]);
+};
