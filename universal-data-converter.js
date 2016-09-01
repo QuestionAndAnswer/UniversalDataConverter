37699bf4f5123c9155f5d7e2f6f70af13c9a1597
Artsiom Miksiuk <artsiom.miksiuk@sap.com>
2016-09-01 23:09:01 +0300

    Direct conversion added.

--- universal-data-converter.js
+++ universal-data-converter.js
@@ -1,89 +1,197 @@
 (function() {
-    /**
-     * UDC class.
-     * @class UDC
-     * @param {object} oParameters Parameters for converter instance construction
-     * @param {array} oParameters.conversions Array of conversions objects
-     */
-    function UDC(oParameters) {
-		jQuery.extend(true, this, oParameters);
-    };
+	var utils = {
+		/**
+		 * Index items by key. Use this in case if keys are unique.
+		 * @prop {array} aList List of items that should be indexed
+		 * @prop {string|function} vKey Poperty name or function which will be used for indexing.
+		 * Function prototype: function (oItem, iIndex, aList).
+		 * @return {object} Indexed by key list
+		 */
+		indexBy: function (aList, vKey) {
+			var i, len, fnKey, oResult = {};
 
-    UDC.prototype.direct = function(oData) {
+			if(typeof vKey === "string") {
+				fnKey = function (oItem) { return oItem[vKey]; };
+			} else {
+				fnKey = vKey;
+			}
 
-    };
+			for(i = 0, len = aList.length; i < len; i++) {
+				oResult[fnKey(aList[i], i, aList)] = aList[i];
+			}
 
-    UDC.prototype.reverse = function(oData) {
+			return oResult;
+		},
 
-    };
+		/**
+		 * Group array's objects using key
+		 * @prop {array} aList List of items that should be grouped
+		 * @prop {string|function} vKey Poperty name or function which will be used for grouping.
+		 * Function prototype: function (oItem, iIndex, aList).
+		 * @return {object} Grouped by key list
+		 */
+		groupBy: function (aList, vKey) {
+			var i, len, fnKey, oResult = {};
 
-	/**
-	 * UTILS
-	 */
+			if(typeof vKey === "string") {
+				fnKey = function (oItem) { return oItem[vKey]; };
+			} else {
+				fnKey = vKey;
+			}
 
-	if(!jQuery) { jQuery = {}; }
-	/**
-	 * This function is a part of jQuery library, and was tooked from jQuery library.
-	 * Copyright jQuery Foundation and other contributors, https://jquery.org/
-	 */
-    jQuery.extend = jQuery.fn.extend = function() {
-        var options, name, src, copy, copyIsArray, clone,
-            target = arguments[0] || {},
-            i = 1,
-            length = arguments.length,
-            deep = false;
+			for(i = 0, len = aList.length; i < len; i++) {
+				var sKey = fnKey(aList[i], i, aList);
+				if(oResult[sKey]) {
+					oResult[sKey].push(aList[i]);
+				} else {
+					oResult[sKey] = [aList[i]];
+				}
+			}
 
-        // Handle a deep copy situation
-        if (typeof target === "boolean") {
-            deep = target;
-            target = arguments[1] || {};
-            // skip the boolean and the target
-            i = 2;
-        }
+			return oResult;
+		},
 
-        // Handle case when target is a string or something (possible in deep copy)
-        if (typeof target !== "object" && !jQuery.isFunction(target)) {
-            target = {};
-        }
+		/**
+		 * Truncate path in slice manner
+		 * @method truncatePath
+		 * @param {boolean} bAsArray If true, result will be in array form, false return string
+		 * @param {string} sPath Path to value in object
+		 * @param {number} iFrom Path part number from which path should be used.
+		 * If iFrom parameter passed only, then working as truncate parameter.
+		 * Truncate parameter may be positive and negative number.
+		 * If positive number provided, then path will be truncated
+		 * from the left side on iTruncate path's parts.
+		 * If negative number provided, then path will be truncated
+		 * from the right side on iTruncate path's parts.
+		 * @param {number} iTo Path part number to which path string must be truncated.
+		 * @return {string|array} Processed path
+		 */
+		truncatePath: function () {
+			var bAsArray, sPath, iFrom, iTo;
+			if(typeof arguments[0] === "string") {
+				sPath = arguments[0];
+				iFrom = arguments[1]; iTo = arguments[2];
+			} else {
+				bAsArray = arguments[0]; sPath = arguments[1];
+				iFrom = arguments[2]; iTo = arguments[3];
+			}
 
-        // extend jQuery itself if only one argument is passed
-        if (length === i) {
-            target = this;
-            --i;
-        }
+			var bIsSlashFirst = sPath[0] === "/";
+			var sPathParts = sPath.split("/").slice(bIsSlashFirst ? 1 : 0);
 
-        for (; i < length; i++) {
-            // Only deal with non-null/undefined values
-            if ((options = arguments[i]) != null) {
-                // Extend the base object
-                for (name in options) {
-                    src = target[name];
-                    copy = options[name];
-                    // Prevent never-ending loop
-                    if (target === copy) {
-                        continue;
-                    }
+			if(!iTo && iTo !== 0) { //if iTo not passed as argument
+				if(iFrom < 0) {
+					sPathParts = sPathParts.slice(0, iFrom);
+				} else {
+					sPathParts = sPathParts.slice(iFrom);
+				}
+			} else {
+				sPathParts = sPathParts.slice(iFrom, iTo);
+			}
 
-                    // Recurse if we're merging plain objects or arrays
-                    if (deep && copy && (jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)))) {
-                        if (copyIsArray) {
-                            copyIsArray = false;
-                            clone = src && jQuery.isArray(src) ? src : [];
-                        } else {
-                            clone = src && jQuery.isPlainObject(src) ? src : {};
-                        }
-                        // Never move original objects, clone them
-                        target[name] = jQuery.extend(deep, clone, copy);
-                        // Don't bring in undefined values
-                    } else if (copy !== undefined) {
-                        target[name] = copy;
-                    }
-                }
-            }
-        }
-        // Return the modified object
-        return target;
-    };
+			if(bAsArray) {
+				return sPathParts.length === 1 && sPathParts[0] === "" ? [] : sPathParts;
+			} else {
+				return (bIsSlashFirst ? "/" : "") + sPathParts.join("/");
+			}
+		},
 
-    window.UDC = UDC;
+		/**
+		 * Get path part
+		 * @method getPathPart
+		 * @param {string} sPath Path from which part is needed
+		 * @param {number} iIndex Part index.
+		 * If positive value passed, then part iIndex counting from the left side will be returned
+		 * If negative value passed, then part iIndex counting from the right side will be returned
+		 * @return {string} Path part.
+		 */
+		getPathPart: function (sPath, iIndex) {
+			var aPathParts = utils.truncatePath(true, sPath);
+			return iIndex >= 0 ? aPathParts[iIndex] : aPathParts[aPathParts.length + iIndex];
+		},
+
+		/**
+		 * Get value in object using provided path
+		 * @method getValByPath
+		 * @param {object} oObject Object from which value must be extracted
+		 * @param {string} sPath Path to value in object
+		 * @param {number} iFrom Path part number from which path should be used.
+		 * @param {number} iTo Path part number to which path string must be truncated.
+		 * @see {truncatePath} for additional information
+		 * @example
+		 * var oObject = {
+		 *   a: {
+		 *     b: "a",
+		 *     c: 3
+		 *   },
+		 *   x: 100
+		 * };
+		 * getValByPath(oObject, "/a/b", -1); // { b: "a", c: 3}
+		 * getValByPath(oObject, "/a/b"); // "a"
+		 * @see test/mode/UDC.utils.js for more examples
+		 * @return {any} Value from object
+		 */
+		getValByPath: function(oObject, sPath, iFrom, iTo) {
+			var sPathParts = utils.truncatePath(true, sPath, iFrom, iTo);
+			var oCurrentObject = oObject;
+			for(var i = 0, len = sPathParts.length; i < len; i++) {
+				oCurrentObject = oCurrentObject[sPathParts[i]];
+			}
+
+			return oCurrentObject;
+		},
+
+		setValByPath: function (oObject, sPath, vVal) {
+			var sPathParts = utils.truncatePath(true, sPath);
+			var oCurrentObject = oObject;
+			for(var i = 0, len = sPathParts.length - 1; i < len; i++) {
+				oCurrentObject = oCurrentObject[sPathParts[i]];
+			}
+			oCurrentObject[utils.getLast(sPathParts)] = vVal;
+
+			return oObject;
+		},
+
+		getLast: function (aList) {
+			return aList[aList.length - 1];
+		}
+	};
+
+    var module = {
+		utils: utils,
+
+		/**
+		 * Direct conversion
+		 * @param {boolean} bExtendExisting If true, then passed object will be extended with result of conversions,
+		 * false, then new object will be created and result will be writed to this new object.
+		 * @param {string} sInPath Input path
+		 * @param {string} [sOutPath] Output path. If ommited, input path will be used
+		 * @param {object} oObject Object on which perform conversion
+		 * @param {function} fnProcessor Conversion value processor
+		 * @return {object}  Processed object
+		 */
+		direct: function () {
+			var i = 0,
+				bExtendExisting = typeof arguments[i] === "boolean" ? arguments[i++] : false,
+				sInPath = arguments[i++],
+				sOutPath = typeof arguments[i] === "string" ? arguments[i++] : sInPath,
+				oObject = arguments[i++],
+				fnProcessor = arguments[i++] || function (oItem) { return oItem; };
+
+			var vVal = utils.getValByPath(oObject, sInPath);
+			var oResultObject = bExtendExisting ? oObject : {};
+			utils.setValByPath(oResultObject, sOutPath, fnProcessor(vVal));
+			return oResultObject;
+		},
+
+		/**
+		 * Broadcast conversion
+		 * @return {object}  Processed object
+		 */
+		broadcast: function () {
+			
+		}
+	};
+
+	window.UDC = module;
 })();
